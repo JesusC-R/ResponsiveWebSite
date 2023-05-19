58124fcc6f4b53ebad77cc49d5b910a0cb34a05b
JesusC-R <cuevasrodartej@gmail.com>
2023-05-18 20:24:11 -0600

    Implementation of media queries

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import classNames from "classnames";
 
-import img1 from '../../assets/img/nat-8.jpg';
-import img2 from '../../assets/img/nat-9.jpg';
+import img1 from "../../assets/img/nat-8.jpg";
+import img2 from "../../assets/img/nat-9.jpg";
 
-function PopUp (props) {
+function PopUp(props) {
   const classes = {
     popup: classNames(props.popupClasses),
-    content: classNames('popup__content', props.contantClasses)
-  }
+    content: classNames("popup__content", props.contantClasses),
+  };
   return (
     <div className={classes.popup} id="popup">
       <div className={classes.content}>
-        <div className='popup__left'>
-          <img className='popup__img' src={img1} alt='TourImg' />
-          <img className='popup__img' src={img2} alt='TourImg' />
+        <div className="popup__left">
+          <img className="popup__img" src={img1} alt="TourImg" />
+          <img className="popup__img" src={img2} alt="TourImg" />
         </div>
-        <div className='popup__right'>
-          <a className="popup__close" href="#tours">X</a>
-          <h2 className='heading-secondary u-margin-bottom-medium'>Start booking now</h2>
-          <h3 className='heading-tertiary u-margin-bottom-small'>Importatnt - Please read the terms and services before booking</h3>
-          <p className='popup__text'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus nam nostrum laboriosam quidem doloremque, maiores quisquam, vel ad, amet fuga ducimus dolor enim asperiores! Illum hic explicabo alias repellat rem.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus obcaecati odit facere ex inventore non perspiciatis corporis fuga? Enim cum eligendi esse recusandae quod, veniam nam vitae architecto in molestias.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione asperiores quibusdam dolorum eius neque fugiat nam magnam, accusantium temporibus repudiandae, beatae tempora consequatur est veniam velit commodi esse magni a.
-
+        <div className="popup__right">
+          <a className="popup__close" href="#tours">
+            X
+          </a>
+          <h2 className="heading-secondary u-margin-bottom-medium">
+            Start booking now
+          </h2>
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            Importatnt - Please read the terms and services before booking
+          </h3>
+          <p className="popup__text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Voluptatibus nam nostrum laboriosam quidem doloremque, maiores
+            quisquam, vel ad, amet fuga ducimus dolor enim asperiores! Illum hic
+            explicabo alias repellat rem. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Accusamus obcaecati odit facere ex
+            inventore non perspiciatis corporis fuga? Enim cum eligendi esse
+            recusandae quod, veniam nam vitae architecto in molestias. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Ratione
+            asperiores quibusdam dolorum eius neque fugiat nam magnam,
+            accusantium temporibus repudiandae, beatae tempora consequatur est
+            veniam velit commodi esse magni a.
           </p>
-          <button className='btn btn--green'>Book now</button>
+          <button className="btn btn--green">Book now</button>
         </div>
       </div>
     </div>
--- src/components/layout/stories/Stories.jsx
+++ src/components/layout/stories/Stories.jsx
@@ -1,48 +1,56 @@
-import React from 'react';
+import React from "react";
 // import classNames from 'classnames';
 
-import SubHeading from '../header/heading/SubHeading';
-import StoryCard from './StoryCard';
-import Button from '../../common/Button';
-import BgVideo from '../../common/BgVideo';
+import SubHeading from "../header/heading/SubHeading";
+import StoryCard from "./StoryCard";
+import Button from "../../common/Button";
+import BgVideo from "../../common/BgVideo";
 
-import profileImg01 from '../../../assets/img/nat-8.jpg';
-import profileImg02 from '../../../assets/img/nat-9.jpg';
-import video01 from '../../../assets/img/video.mp4';
-import video02 from '../../../assets/img/video.webm'
+import profileImg01 from "../../../assets/img/nat-8.jpg";
+import profileImg02 from "../../../assets/img/nat-9.jpg";
+import video01 from "../../../assets/img/video.mp4";
+import video02 from "../../../assets/img/video.webm";
 
-function Stories (props) {
+function Stories(props) {
   return (
     <section className={props.storiesClasses}>
-      <BgVideo 
+      <BgVideo
         video01={video01}
-        videoType01={'video/mp4'}
+        videoType01={"video/mp4"}
         video02={video02}
-        videoType02={'video/webm'}
+        videoType02={"video/webm"}
       />
 
-      <SubHeading 
-        textBoxClasses={'u-center-text u-margin-bottom-big u-margin-top-big'}
-        textClasses={'heading-secondary'}
-        text={'We make people genuinly happy!'}
+      <SubHeading
+        textBoxClasses={"u-center-text u-margin-bottom-big u-margin-top-big"}
+        textClasses={"heading-secondary"}
+        text={"We make people genuinly happy!"}
       />
 
-      <div className='row'>
-        <StoryCard 
-          subHeading={'I had the best week ever with my family'}
-          text={'lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetumlorem ipsum dolor sit amet, consectlorem ipsum dolor sit amet, consectetumetum lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetumlorem ipsum dolor sit amet, consectlorem ipsum dolor sit amet, consectetumetum'}
+      <div className="row">
+        <StoryCard
+          subHeading={"I had the best week ever with my family"}
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip"
+          }
           profileImg={profileImg01}
-          name={'Mary Smith'}
+          name={"Mary Smith"}
         />
-        <StoryCard 
-          subHeading={'WOW! My life is completely dofferent now'}
-          text={'lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetumlorem ipsum dolor sit amet, consectlorem ipsum dolor sit amet, consectetumetum lorem ipsum dolor sit amet, consectetum lorem ipsum dolor sit amet, consectetumlorem ipsum dolor sit amet, consectlorem ipsum dolor sit amet, consectetumetum'}
+        <StoryCard
+          subHeading={"WOW! My life is completely dofferent now"}
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip"
+          }
           profileImg={profileImg02}
-          name={'Jack Wilson'}
-        /> 
-        <div className='u-center-text u-margin-top-big'>
-            <Button buttonClasses={'btn-text'} href={'#'} text={'Read all stories -->'} />
-        </div>        
+          name={"Jack Wilson"}
+        />
+        <div className="u-center-text u-margin-top-big">
+          <Button
+            buttonClasses={"btn-text"}
+            href={"#"}
+            text={"Read all stories -->"}
+          />
+        </div>
       </div>
     </section>
   );
